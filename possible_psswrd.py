6bb68ae10de27120b8e1e350abaccf081eced283
Direnzi <98608748+Direnzii@users.noreply.github.com>
2025-04-09 18:35:35 -0300

    refactor: refatoração para deixar mais legivel e sem repetições

--- possible_psswrd.py
+++ possible_psswrd.py
@@ -6,292 +6,73 @@ def retornar_lista_parametro(parametros_string):
 
 
 def get_dict_par():
-    parametro_list = retornar_lista_parametro(input('insira os parametros como nome de variavel, '
-                                                    'dividindo por ","\nEX: data_nascimento, primeiro_nome, '
-                                                    'mid_nome\nNão popular as variaveis com espaço\n'
-                                                    'EX: data_nascimento = 20032000 com todos os '
-                                                    'caracteres: '))
-    dict_var = {}
-    for i in parametro_list:
-        dict_var[i] = popular_var(i)
-    return dict_var
+    parametros_string = input('Insira os parâmetros como nome de variável, separados por vírgula (Ex: data_nascimento, primeiro_nome, mid_nome): ')
+    parametro_list = retornar_lista_parametro(parametros_string)
+    return {param: popular_var(param) for param in parametro_list}
 
 
-def popular_var(show_in_input):
-    return input(f'{show_in_input}: ')
+def popular_var(param):
+    return input(f'Valor para {param}: ')
 
 
 def get_opcoes(dic_opcionais, dict_principal):
     qtd_parametro = dic_opcionais['qtd_de_parametros_usados']
-    combinacoes = concatenate_dict_values(dict_principal, qtd_parametro)
-    return combinacoes
+    return concatenate_dict_values(dict_principal, qtd_parametro)
 
 
-def primeira_letra_maiuscula(string):
-    return string.capitalize()
+def primeira_letra_maiuscula(s):
+    return s.capitalize()
 
 
-def full_maiusculo(string):
-    return string.upper()
+def full_maiusculo(s):
+    return s.upper()
+
+
+def generate_combinations(values, num_values):
+    combs = itertools.combinations(values, num_values)
+    results = set()  # Usamos set para evitar duplicações
+
+    for comb in combs:
+        results.update(generate_all_combinations(comb))
+    
+    return list(results)
+
+
+def generate_all_combinations(comb):
+    value_combinations = []
+    for perm in itertools.permutations(comb):
+        value_combinations.extend(generate_variants(perm))
+    return value_combinations
+
+
+def generate_variants(perm):
+    v1, v2 = perm
+    v1_capt, v2_capt = primeira_letra_maiuscula(v1), primeira_letra_maiuscula(v2)
+    v1_upper, v2_upper = full_maiusculo(v1), full_maiusculo(v2)
+
+    variants = [
+        v1 + v2, v1_capt + v2, v1 + v2_capt, v1_upper + v2, v1_upper + v2_capt,
+        v1_upper + v2_upper, v2 + v1, v2_capt + v1, v2_upper + v1, v2_upper + v1_capt,
+        v2_upper + v1_upper, v2_capt + v1_upper, v1 + v2_upper, v1_capt + v2_upper,
+        v1_upper + v2_upper
+    ]
+    
+    return set(variants)  # Usamos set para evitar duplicações
 
 
 def concatenate_dict_values(dict_values, num_values):
-    results = []
-    if num_values == 2:
-        combs = list(itertools.combinations(dict_values.keys(), 2))
-        for key1, key2 in combs:
-            value1 = dict_values[key1]
-            value2 = dict_values[key2]
-            value1_capt = primeira_letra_maiuscula(value1)
-            value2_capt = primeira_letra_maiuscula(value2)
-            value1_upper = full_maiusculo(value1)
-            value2_upper = full_maiusculo(value2)
-            results.append(value1 + value2)
-            if not value1_capt + value2 in results:
-                results.append(value1_capt + value2)
-            if not value1 + value2_capt in results:
-                results.append(value1 + value2_capt)
-            if not value1_upper + value2 in results:
-                results.append(value1_upper + value2)
-            if not value1_upper + value2_capt in results:
-                results.append(value1_upper + value2_capt)
-            if not full_maiusculo(value1_upper + value2_upper) in results:
-                results.append(value1_upper + value2_upper)
-            if not value2_upper + value1 in results:
-                results.append(value2_upper + value1)
-            if not value2_upper + value1_capt in results:
-                results.append(value2_upper + value1_capt)
-            if not value2_upper + value1_upper in results:
-                results.append(value2_upper + value1_upper)
-            if not value2 + value1_upper in results:
-                results.append(value2 + value1_upper)
-            if not value2_capt + value1_upper in results:
-                results.append(value2_capt + value1_upper)
-            if not value2_upper + value1_upper in results:
-                results.append(value2_upper + value1_upper)
-            if not value1 + value2_upper in results:
-                results.append(value1 + value2_upper)
-            if not value1_capt + value2_upper in results:
-                results.append(value1_capt + value2_upper)
-            if not value1_upper + value2_upper in results:
-                results.append(value1_upper + value2_upper)
-            if not value2_capt + value1 in results:
-                results.append(value2_capt + value1)
-            if not value2 + value1_capt in results:
-                results.append(value2 + value1_capt)
-        return results
-    if num_values == 3:
-        combs = list(itertools.combinations(dict_values.keys(), 3))
-        for key1, key2, key3 in combs:
-            value1 = dict_values[key1]
-            value2 = dict_values[key2]
-            value3 = dict_values[key3]
-            value1_capt = primeira_letra_maiuscula(value1)
-            value2_capt = primeira_letra_maiuscula(value2)
-            value3_capt = primeira_letra_maiuscula(value3)
-            value1_upper = full_maiusculo(value1)
-            value2_upper = full_maiusculo(value2)
-            value3_upper = full_maiusculo(value3)
-            results.append(value1 + value2 + value3)
-            results.append(value1 + value3 + value2)
-            results.append(value2 + value1 + value3)
-            results.append(value2 + value3 + value1)
-            results.append(value3 + value1 + value2)
-            results.append(value3 + value2 + value1)
-            if not value1_capt + value2 + value3 in results:
-                results.append(value1_capt + value2 + value3)
-            if not value1 + value2_capt + value3 in results:
-                results.append(value1 + value2_capt + value3)
-            if not value1 + value2 + value3_capt in results:
-                results.append(value1 + value2 + value3_capt)
-            if not value1_upper + value2 + value3 in results:
-                results.append(value1_upper + value2 + value3)
-            if not value1_upper + value2_capt + value3 in results:
-                results.append(value1_upper + value2_capt + value3)
-            if not value1_upper + value2 + value3_capt in results:
-                results.append(value1_upper + value2 + value3_capt)
-            if not value1_upper + value2_upper + value3 in results:
-                results.append(value1_upper + value2_upper + value3)
-            if not value1_upper + value2_upper + value3_capt in results:
-                results.append(value1_upper + value2_upper + value3_capt)
-            if not value1_upper + value2_capt + value3_upper in results:
-                results.append(value1_upper + value2_capt + value3_upper)
-            if not value1_upper + value2_capt + value3_capt in results:
-                results.append(value1_upper + value2_capt + value3_capt)
-            if not value2_upper + value1 + value3 in results:
-                results.append(value2_upper + value1 + value3)
-            if not value2_upper + value1_capt + value3 in results:
-                results.append(value2_upper + value1_capt + value3)
-            if not value2_upper + value1_upper + value3 in results:
-                results.append(value2_upper + value1_upper + value3)
-            if not value2 + value1_upper + value3 in results:
-                results.append(value2 + value1_upper + value3)
-            if not value2_capt + value1_upper + value3 in results:
-                results.append(value2_capt + value1_upper + value3)
-            if not value2 + value1_capt + value3 in results:
-                results.append(value2 + value1_capt + value3)
-            if not value2_capt + value1 + value3 in results:
-                results.append(value2_capt + value1 + value3)
-            if not value2 + value1 + value3_capt in results:
-                results.append(value2 + value1 + value3_capt)
-            if not value2_capt + value1_upper + value3_upper in results:
-                results.append(value2_capt + value1_upper + value3_upper)
-            if not value2_upper + value1_capt + value3_capt in results:
-                results.append(value2_upper + value1_capt + value3_capt)
-            if not value2_upper + value1_upper + value3 in results:
-                results.append(value2_upper + value1_upper + value3)
-            if not value2_upper + value1_upper + value3_capt in results:
-                results.append(value2_upper + value1_upper + value3_capt)
-            if not value2_upper + value1_capt + value3_upper in results:
-                results.append(value2_upper + value1_capt + value3_upper)
-            if not value2_upper + value1_upper + value3_upper in results:
-                results.append(value2_upper + value1_upper + value3_upper)
-            if not value2_upper + value3 + value1 in results:
-                results.append(value2_upper + value3 + value1)
-            if not value2_upper + value3_capt + value1 in results:
-                results.append(value2_upper + value3_capt + value1)
-            if not value2_upper + value3_upper + value1 in results:
-                results.append(value2_upper + value3_upper + value1)
-            if not value2 + value1_upper + value3_capt in results:
-                results.append(value2 + value1_upper + value3_capt)
-            if not value2 + value1_capt + value3_upper in results:
-                results.append(value2 + value1_capt + value3_upper)
-            if not value2 + value1_upper + value3_upper in results:
-                results.append(value2 + value1_upper + value3_upper)
-            if not value2 + value3_upper + value1_upper in results:
-                results.append(value2 + value3_upper + value1_upper)
-            if not value2_capt + value1_upper + value3 in results:
-                results.append(value2_capt + value1_upper + value3)
-            if not value2_capt + value1_upper + value3_capt in results:
-                results.append(value2_capt + value1_upper + value3_capt)
-            if not value2_capt + value1_capt + value3_upper in results:
-                results.append(value2_capt + value1_capt + value3_upper)
-            if not value2_capt + value1_capt + value3_capt in results:
-                results.append(value2_capt + value1_capt + value3_capt)
-            if not value2_capt + value1_capt + value3 in results:
-                results.append(value2_capt + value1_capt + value3)
-            if not value2_capt + value3 + value1_upper in results:
-                results.append(value2_capt + value3 + value1_upper)
-            if not value2_capt + value3_upper + value1 in results:
-                results.append(value2_capt + value3_upper + value1)
-            if not value2_capt + value3_upper + value1_upper in results:
-                results.append(value2_capt + value3_upper + value1_upper)
-            if not value2 + value1_upper + value3 in results:
-                results.append(value2 + value1_upper + value3)
-            if not value2 + value1_upper + value3_capt in results:
-                results.append(value2 + value1_upper + value3_capt)
-            if not value2 + value1_capt + value3_upper in results:
-                results.append(value2 + value1_capt + value3_upper)
-            if not value2_capt + value1 + value3_upper in results:
-                results.append(value2_capt + value1 + value3_upper)
-            if not value2_capt + value1_upper + value3_upper in results:
-                results.append(value2_capt + value1_upper + value3_upper)
-            if not value2_upper + value1 + value3_capt in results:
-                results.append(value2_upper + value1 + value3_capt)
-            if not value2_upper + value1_capt + value3 in results:
-                results.append(value2_upper + value1_capt + value3)
-            if not value2_upper + value1_capt + value3_capt in results:
-                results.append(value2_upper + value1_capt + value3_capt)
-            if not value2_upper + value1_upper + value3 in results:
-                results.append(value2_upper + value1_upper + value3)
-            if not value2_upper + value1_upper + value3_capt in results:
-                results.append(value2_upper + value1_upper + value3_capt)
-            if not value3 + value1 + value2 in results:
-                results.append(value3 + value1 + value2)
-            if not value3_capt + value1 + value2 in results:
-                results.append(value3_capt + value1 + value2)
-            if not value3_upper + value1 + value2 in results:
-                results.append(value3_upper + value1 + value2)
-            if not value3 + value1_capt + value2 in results:
-                results.append(value3 + value1_capt + value2)
-            if not value3_capt + value1_capt + value2 in results:
-                results.append(value3_capt + value1_capt + value2)
-            if not value3_upper + value1_capt + value2 in results:
-                results.append(value3_upper + value1_capt + value2)
-            if not value3 + value1_upper + value2 in results:
-                results.append(value3 + value1_upper + value2)
-            if not value3_capt + value1_upper + value2 in results:
-                results.append(value3_capt + value1_upper + value2)
-            if not value3_upper + value1_upper + value2 in results:
-                results.append(value3_upper + value1_upper + value2)
-            if not value3 + value2 + value1_upper in results:
-                results.append(value3 + value2 + value1_upper)
-            if not value3_capt + value2 + value1_upper in results:
-                results.append(value3_capt + value2 + value1_upper)
-            if not value3_upper + value2 + value1_upper in results:
-                results.append(value3_upper + value2 + value1_upper)
-            if not value3 + value2_capt + value1_upper in results:
-                results.append(value3 + value2_capt + value1_upper)
-            if not value3_capt + value2_capt + value1_upper in results:
-                results.append(value3_capt + value2_capt + value1_upper)
-            if not value3_capt + value1 + value2_upper in results:
-                results.append(value3_capt + value1 + value2_upper)
-            if not value3_capt + value1_upper + value2_upper in results:
-                results.append(value3_capt + value1_upper + value2_upper)
-            if not value3_capt + value2_upper + value1_upper in results:
-                results.append(value3_capt + value2_upper + value1_upper)
-            if not value3_upper + value1 + value2 in results:
-                results.append(value3_upper + value1 + value2)
-            if not value3_upper + value1_capt + value2 in results:
-                results.append(value3_upper + value1_capt + value2)
-            if not value3_upper + value1 + value2_capt in results:
-                results.append(value3_upper + value1 + value2_capt)
-            if not value3_upper + value1_capt + value2_capt in results:
-                results.append(value3_upper + value1_capt + value2_capt)
-            if not value3_upper + value2 + value1 in results:
-                results.append(value3_upper + value2 + value1)
-            if not value3_upper + value2_capt + value1 in results:
-                results.append(value3_upper + value2_capt + value1)
-            if not value3_upper + value2 + value1_capt in results:
-                results.append(value3_upper + value2 + value1_capt)
-            if not value3_upper + value2_capt + value1_capt in results:
-                results.append(value3_upper + value2_capt + value1_capt)
-        return results
-    else:
-        results = []
-        for i, key in enumerate(dict_values.keys()):
-            sub_dict_values = \
-                {key_var: value for indice, (key_var, value) in enumerate(dict_values.items()) if indice > i}
-            sub_results = concatenate_dict_values(sub_dict_values, num_values - 1)
-            for sub_result in sub_results:
-                value1 = dict_values[key]
-                value2 = sub_result
-                var_1_capt = primeira_letra_maiuscula(value1)
-                var_2_capt = primeira_letra_maiuscula(value2)
-                var_1_full = full_maiusculo(value1)
-                var_2_full = full_maiusculo(value2)
-                if not value1 + value2 in results:
-                    results.append(value1 + value2)
-                if not value2 + value1 in results:
-                    results.append(value2 + value1)
-                if not var_1_capt + value2 in results:
-                    results.append(var_1_capt + value2)
-                if not value2 + var_1_capt in results:
-                    results.append(value2 + var_1_capt)
-                if not var_1_full + value2 in results:
-                    results.append(var_1_full + value2)
-                if not var_2_full + value1 in results:
-                    results.append(var_2_full + value1)
-                if not var_1_full + var_2_capt in results:
-                    results.append(var_1_full + var_2_capt)
-                if not var_2_full + var_1_capt in results:
-                    results.append(var_2_full + var_1_capt)
-                if not var_1_full + var_2_full in results:
-                    results.append(var_1_full + var_2_full)
-                if not var_2_full + var_1_full in results:
-                    results.append(var_2_full + var_1_full)
-        return results
+    if num_values < 2 or num_values > 3:
+        return generate_combinations(dict_values.values(), num_values)
+    return generate_combinations(dict_values.values(), num_values)
 
 
 def rodar(return_list=True):
     dict_parametros_opcionais = {
-        "qtd_de_parametros_usados": int(input('Quantidade de parametros usados (int): '))
+        "qtd_de_parametros_usados": int(input('Quantidade de parâmetros usados (int): '))
     }
     parametros_dict = get_dict_par()
+    opcoes = get_opcoes(dict_parametros_opcionais, parametros_dict)
+    
     if return_list:
-        return get_opcoes(dict_parametros_opcionais, parametros_dict)
-    if not return_list:
-        saida = ''
-        for i in get_opcoes(dict_parametros_opcionais, parametros_dict):
-            saida += i + '\n'
-        return saida
+        return opcoes
+    return "\n".join(opcoes)
